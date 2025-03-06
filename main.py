082a2e704aab356183950f67604dd7a640ffed00
YoussefSNK <myoussef.fadili@gmail.com>
2025-03-06 01:54:36 +0100

    feat: background qui défile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 1000
 FPS = 60
@@ -8,13 +9,47 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 
+class Background:
+    def __init__(self, image_path=None, speed=2):
+        self.speed = speed
+        if image_path:
+            self.image = pygame.image.load(image_path).convert()
+            self.image = pygame.transform.scale(self.image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        else:
+            self.image = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+            self.image.fill((10, 10, 30))
+            for _ in range(100):
+                x = random.randint(0, SCREEN_WIDTH)
+                y = random.randint(0, SCREEN_HEIGHT)
+                pygame.draw.circle(self.image, (255, 255, 255), (x, y), 1)
+        self.y1 = 0
+        self.y2 = -SCREEN_HEIGHT
+
+    def update(self):
+        self.y1 += self.speed
+        self.y2 += self.speed
+        if self.y1 >= SCREEN_HEIGHT:
+            self.y1 = -SCREEN_HEIGHT
+        if self.y2 >= SCREEN_HEIGHT:
+            self.y2 = -SCREEN_HEIGHT
+
+    def draw(self, surface):
+        surface.blit(self.image, (0, self.y1))
+        surface.blit(self.image, (0, self.y2))
+
+
 class Level:
     def __init__(self):
+        self.background = Background(speed=2)
         self.timer = 0
         self.enemies = []
 
     def update(self):
+        self.background.update()
         self.timer += 1
+
+    def draw(self, surface):
+        self.background.draw(surface)
 
 class Projectile:
     def __init__(self, x, y, speed=10):
@@ -76,6 +111,7 @@ def main():
         projectiles = [p for p in projectiles if p.rect.bottom > 0]
 
         screen.fill(BLACK)
+        level.draw(screen)
         for projectile in projectiles:
             projectile.draw(screen)
         player.draw(screen)
